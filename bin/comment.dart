e87bff75f997c6074ce92fc836459f445a104c32
Nur Muhammad Shufi <shufimuhammad013@gmail.com>
2023-03-28 01:23:23 +0700

    comment dart

---
+++ bin/comment.dart
@@ -0,0 +1,15 @@
+/// this is main function
+/// will execute first
+void main(List<String> args) {
+  // this is variable name
+  var name = "Shufi";
+
+  /**
+   * multiple print
+   * variable name 
+   */
+
+  print(name);
+  print(name);
+  print(name);
+}
