8288ec05b7196fcd5f1c4dec0cb5d39d764422ad
Nur Muhammad Shufi <shufimuhammad013@gmail.com>
2023-03-28 02:21:36 +0700

    operasi aritmatika

---
+++ bin/operasi_Aritmatika.dart
@@ -0,0 +1,17 @@
+void main(List<String> args) {
+  var result0 = 10;
+  var result1 = 10 + 10;
+  var result2 = 10 - 10;
+  var result3 = 10 * 10;
+  var result4 = 10 / 10;
+  var result5 = 10 ~/ 10;
+  var result6 = 10 % 10;
+
+  print("Variable $result0");
+  print("10 + 10 = $result1");
+  print("10 - 10 = $result2");
+  print("10 * 10 = $result3");
+  print("10 / 10 = $result4");
+  print("10 ~/ 10 = $result5");
+  print("10 % 10 = $result6");
+}
